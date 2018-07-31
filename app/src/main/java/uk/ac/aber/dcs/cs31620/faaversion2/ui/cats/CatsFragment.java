a958bf8e652c5ec64a997b5238b7d8780e9f5af6
Chris Loftus <cwl@aber.ac.uk>
2018-07-31 09:02:44 +0100

    Fixed the layout name in CatsFragment

--- app/src/main/java/uk/ac/aber/dcs/cs31620/faaversion2/ui/cats/CatsFragment.java
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faaversion2/ui/cats/CatsFragment.java
@@ -23,7 +23,7 @@ public class CatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_cats, container, false);
     }
 
 
